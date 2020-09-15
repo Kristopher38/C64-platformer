ab930d5d1a2d5619a792b6ed7c5967aa7198f4e5
Kristopher38 <kristopher38@wp.pl>
2020-09-15 19:41:44 +0200

    Fixed smooth scrolling, added comments and indentation

--- scroll.asm
+++ scroll.asm
@@ -11,114 +11,125 @@
 scrbuf1 = $0400
 scrbuf2 = $0800
 
-jsr initScreen
+main:
+    jsr initScreen
 
-sei         ; set interrupt disable flag
+    sei         ; set interrupt disable flag
 
-ldy #$7f    ; $7f = %01111111
-sty $dc0d   ; Turn off CIAs Timer interrupts
-sty $dd0d   ; Turn off CIAs Timer interrupts
-lda $dc0d   ; cancel all CIA-IRQs in queue/unprocessed
-lda $dd0d   ; cancel all CIA-IRQs in queue/unprocessed
+    ldy #$7f    ; $7f = %01111111
+    sty $dc0d   ; Turn off CIAs Timer interrupts
+    sty $dd0d   ; Turn off CIAs Timer interrupts
+    lda $dc0d   ; cancel all CIA-IRQs in queue/unprocessed
+    lda $dd0d   ; cancel all CIA-IRQs in queue/unprocessed
 
-lda #$01    ; Set Interrupt Request Mask...
-sta $d01a   ; ...we want IRQ by Rasterbeam
+    lda #$01    ; Set Interrupt Request Mask...
+    sta $d01a   ; ...we want IRQ by Rasterbeam
 
-lda #<irq   ; point IRQ Vector to our custom irq routine
-ldx #>irq 
-sta $314    ; store in $314/$315
-stx $315   
+    lda #<irq   ; point IRQ Vector to our custom irq routine
+    ldx #>irq 
+    sta $314    ; store in $314/$315
+    stx $315   
 
-lda #$00    ; trigger first interrupt at row zero
-sta $d012
+    lda #$00    ; trigger first interrupt at row zero
+    sta $d012
 
-lda $d011   ; Bit#0 of $d011 is basically...
-and #$7f    ; ...the 9th Bit for $d012
-sta $d011   ; we need to make sure it is set to zero 
+    lda $d011   ; Bit#0 of $d011 is basically...
+    and #$7f    ; ...the 9th Bit for $d012
+    sta $d011   ; we need to make sure it is set to zero 
 
-cli         ; clear interrupt disable flag
-jmp *       ; infinite loop
+    cli         ; clear interrupt disable flag
+    jmp *       ; infinite loop
+
+initScreen:
+    lda #$00
+    sta $d020
+    sta $d021
+    rts
 
 ;============================================================
 ;    custom interrupt routine
 ;============================================================
 
-.xscroll !fill 1, $07
+.xscroll !fill 1, $08
 irq:
-dec $d019        ; acknowledge IRQ
+    dec $d019        ; acknowledge IRQ
 
-lda #$0a
-sta $d020
+    ;lda #$0a
+    ;sta $d020   ; change frame color for assesing how much time interrupt took
 
-lda .xscroll
-cmp #$07
-beq .blit
-jmp .decscroll
+    jsr decxscroll
+    lda .xscroll
+    beq .shiftOneChar ; we cycled through values 7 to 0 so we need to shift the whole screen one char position
+    cmp #$07
+    beq .swapBuf    ; if xscroll is 7 we shifted all chars by one position one irq before
+                    ; so we need to change the displayed buffer
+    jmp .exit
 
-.blit
-lda currentCycle
-bne .secondCycle
-lda $d018
-and #!$20
-ora #$10
-sta $d018
-jmp .decscroll
+.swapBuf
+    jsr swapBuffers
+    jmp .exit
 
-.secondCycle
-lda $d018
-and #!$10
-ora #$20
-sta $d018
-
-.decscroll
-dec .xscroll
-beq .shift
-lda $d016
-and #$f8
-clc
-adc .xscroll
-sta $d016
-jmp .exit
-
-.shift
-lda #$07
-sta .xscroll
-jsr shiftScreen
+.shiftOneChar
+    lda #$08
+    sta .xscroll
+    jsr shiftScreen ; and shift entire screen by one character
 
 .exit
-lda #$05
-sta $d020
+    ;lda #$05
+    ;sta $d020   ; reset frame color for assesing how much time irq took
+    jmp $ea81   ; return to kernel interrupt routine
 
-jmp $ea81        ; return to kernel interrupt routine
+decxscroll:
+    dec .xscroll
+    lda $d016   ; if not, we load a register that holds x scroll offset
+    and #$f8    ; mask off appropriate values (4 most significant bits)
+    clc
+    adc .xscroll    ; add our scroll value
+    sta $d016   ; and store it back
+    rts
 
-
-initScreen:
-;lda $d016
-;ora #$07
-rts
+!zone swapBuffers
+swapBuffers:
+    lda currentCycle
+    bne .secondCycle
+    lda $d018
+    and #!$20   ; turn off bit for buffer at $0400
+    ora #$10    ; turn on bit for buffer at $0800
+    sta $d018
+    jmp .exit
+.secondCycle
+    lda $d018
+    and #!$10   ; turn off bit for buffer at $0800
+    ora #$20    ; turn on bit for buffer at $0400
+    sta $d018
+.exit:
+    rts
 
 !zone shiftScreen
-currentCycle !fill 1, $00
+currentCycle !fill 1, $00   ; 0 or 1, depends on current front and backbuffer
 shiftScreen:
-lda currentCycle
-bne .secondCycle
+    lda currentCycle
+    bne .secondCycle
 
-jsr shiftToBuf2
-lda #$01
-sta currentCycle
-jmp .exit
+    ; call appropriate subroutine for shifting screen to an appropriate framebuffer
+    jsr shiftToBuf2
+    lda #$01
+    sta currentCycle    ; change current cycle to opposite
+    jmp .exit
 
 .secondCycle
-lda #$00
-sta currentCycle
-jsr shiftToBuf1
+    lda #$00
+    sta currentCycle    ; change current cycle to opposite
+    jsr shiftToBuf1
 
 .exit
-rts
+    rts
 
 shiftToBuf2:
-ldx #$00
+    ldx #$00
 .loop
+    ; copy characters from scrbuf1 to scrbuf2 offset one character to the left
+    ; disregard characters that would wrap around from the right for now
     lda scrbuf1 + $001, x
     sta scrbuf2 + $000, x
     lda scrbuf1 + $101, x
@@ -130,16 +141,18 @@ ldx #$00
     inx
     bne .loop
 
-!for i, 0, 24 {
-    lda scrbuf1 + i * 40
-    sta scrbuf2 - 1 + (i + 1) * 40
-}
-rts
+    ; correctly copy characters that should wrap around, from the left edge to the the right edge
+    !for i, 0, 24 {
+        lda scrbuf1 + i * 40
+        sta scrbuf2 - 1 + (i + 1) * 40
+    }
+    rts
 
 !zone shiftToBuf1
 shiftToBuf1:
-ldx #$00
+    ldx #$00
 .loop
+    ; same as above but copies from scrbuf2 to scrbuf1
     lda scrbuf2 + $001, x
     sta scrbuf1 + $000, x
     lda scrbuf2 + $101, x
@@ -151,19 +164,19 @@ ldx #$00
     inx
     bne .loop
 
-!for i, 0, 24 {
-    lda scrbuf2 + i * 40
-    sta scrbuf1 - 1 + (i + 1) * 40
-}
-rts
+    !for i, 0, 24 {
+        lda scrbuf2 + i * 40
+        sta scrbuf1 - 1 + (i + 1) * 40
+    }
+    rts
 
 !zone delay
 delay:
-ldx $00
+    ldx $00
 .loop
     !for i, 0, 5 {
         nop
     }
     inx
     bne .loop
-rts
+    rts
